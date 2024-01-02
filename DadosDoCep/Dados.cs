19c613b208316d7cd0229416eff077a885cbdfdc
thiagomiyaguchi <89756000+thiagomiyaguchi@users.noreply.github.com>
2024-01-02 17:37:02 -0300

    first commit

---
+++ DadosDoCep/Dados.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+namespace ConsumindoApi.DadosDoCep;
+
+internal class Dados
+{
+    [JsonPropertyName("logradouro")]
+    public string Logradouro { get; set; }
+
+    [JsonPropertyName("bairro")]
+    public string Bairro { get; set; }
+
+    [JsonPropertyName("localidade")]
+    public string Cidade { get; set; }
+
+    [JsonPropertyName("uf")]
+    public string Estado { get; set; }
+
+    public void ExibirDadosDoCep()
+    {
+        Console.WriteLine($"Este CEP refere-se ao logradouro {Logradouro}," +
+            $" no bairro {Bairro} da cidade de {Cidade}-{Estado}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsumindoApi.DadosDoCep;
+using System.Text.Json;
+
+string resultado;
+Console.Write("Digite o CEP para conhecer o endereço: ");
+string cep = Console.ReadLine()!;
+string url = $"https://viacep.com.br/ws/{cep}/json/";
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        resultado = await client.GetStringAsync(url);
+        var dadosDoCep = JsonSerializer.Deserialize<Dados>(resultado);
+        dadosDoCep.ExibirDadosDoCep();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+    }
+
+}
+
